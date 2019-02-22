4bfd4cd765a84b2a4d744676b46b6c983c5e31ef
Sultan Alsawaf <sultanxda@gmail.com>
2019-02-22 01:24:12 +0000

    msm: kgsl: Increase worker thread priority
    Currently, the kgsl worker thread is erroneosly ranked right below
    Android's audio threads in terms of priority.
    
    The kgsl worker thread is in the critical path for rendering frames to the
    display, so increase its priority to match the priority of MDSS' kthread (mdss_fb0).
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4949,7 +4949,6 @@ static void kgsl_core_exit(void)
 static int __init kgsl_core_init(void)
 {
 	int result = 0;
-
 	struct sched_param param = { .sched_priority = 16 };
 
 	/* alloc major and minor device numbers */
