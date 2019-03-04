280850d12316568fb455680a265ed2f034bdc165
Michael Bestas <mkbestas@lineageos.org>
2019-03-04 21:12:23 +0200

    ASoC: msm-cpe: Fix msm_cpe_lsm_ioctl_compat error handling
    Change-Id: Iec6a12aaed4dce60f774c09264b8e566a17ce9d9

--- sound/soc/msm/msm-cpe-lsm.c
+++ sound/soc/msm/msm-cpe-lsm.c
@@ -2654,7 +2654,7 @@ static int msm_cpe_lsm_ioctl_compat(struct snd_pcm_substream *substream,
 					"SNDRV_LSM_EVENT_STATUS_V3_32",
 					err);
 				kfree(event_status);
-				err = -EFAULT;
+
 				goto done;
 			}
 		}
