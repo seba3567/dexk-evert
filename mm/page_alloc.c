2bf5cdd4cb2778774b16eae38e0b691d7e0fbb68
seba_3567 <sebastian26481@gmail.com>
2020-05-08 00:27:56 -0400

    mm: Boost when memory pressure becomes high
    When memory pressure starts to balloon, allocation latency takes a hit
    as the page allocator continuously tries to find a way to free up some
    pages. Boosting when this occurs can significantly help reduce
    allocation latency, preventing noticeable lag from being observed.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -62,6 +62,7 @@
 #include <linux/sched/rt.h>
 #include <linux/page_owner.h>
 #include <linux/kthread.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 
 #include <asm/sections.h>
@@ -3278,6 +3279,10 @@ retry:
 			&& !(current->flags & PF_KTHREAD))
 			goto nopage;
 	}
+
+          /* Boost when memory is low so allocation latency doesn't get too bad */
+	cpu_input_boost_kick_max(100);
+	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
 
 	/*
 	 * It can become very expensive to allocate transparent hugepages at
