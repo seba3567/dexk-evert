3a04ee64f9338d37d51994fe096fe52057337b56
seba3567 <sebastian26481@gmail.com>
2020-07-07 22:06:26 -0400

    f2fs fix masive deadblock on last random patch

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -25,7 +25,6 @@
 #include <crypto/hash.h>
 #include <linux/writeback.h>
 #include <linux/overflow.h>
-#include <linux/wait.h>
 
 #define __FS_HAS_ENCRYPTION IS_ENABLED(CONFIG_F2FS_FS_ENCRYPTION)
 #include <linux/fscrypt.h>
