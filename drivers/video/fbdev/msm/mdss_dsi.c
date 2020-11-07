dd7f77b4cd9f61f717d469203d585d275f342d5b
seba3567 <sebastian26481@gmail.com>
2020-11-06 21:49:07 -0300

    Revert "msm: mdss: remove some more checks to get the tear check working"
    This reverts commit 7df26f0b6aedcdaa5f1baa25b7f9efa08a1c78ff.

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -1741,7 +1741,6 @@ static int mdss_dsi_unblank(struct mdss_panel_data *pdata)
 
 	if ((pdata->panel_info.type == MIPI_CMD_PANEL) &&
 		mipi->vsync_enable && mipi->hw_vsync_mode) {
-
 		mdss_dsi_set_tear_on(ctrl_pdata);
 		if (mdss_dsi_is_te_based_esd(ctrl_pdata))
 			panel_update_te_irq(pdata, true);
@@ -1812,7 +1811,8 @@ static int mdss_dsi_blank(struct mdss_panel_data *pdata, int power_state)
 		}
 	}
 
-	if (mipi->vsync_enable && mipi->hw_vsync_mode) {
+	if ((pdata->panel_info.type == MIPI_CMD_PANEL) &&
+		mipi->vsync_enable && mipi->hw_vsync_mode) {
 		if (mdss_dsi_is_te_based_esd(ctrl_pdata)) {
 			panel_update_te_irq(pdata, false);
 			atomic_dec(&ctrl_pdata->te_irq_ready);
@@ -3844,7 +3844,8 @@ static int mdss_dsi_ctrl_probe(struct platform_device *pdev)
 	pdata = &ctrl_pdata->panel_data;
 	init_completion(&pdata->te_done);
 	mutex_init(&pdata->te_mutex);
-	if (!te_irq_registered) {
+	if (pdata->panel_info.type == MIPI_CMD_PANEL) {
+		if (!te_irq_registered) {
 			rc = devm_request_irq(&pdev->dev,
 				gpio_to_irq(pdata->panel_te_gpio),
 				test_hw_vsync_handler, IRQF_TRIGGER_FALLING,
@@ -3856,6 +3857,7 @@ static int mdss_dsi_ctrl_probe(struct platform_device *pdev)
 			te_irq_registered = 1;
 			disable_irq_nosync(gpio_to_irq(pdata->panel_te_gpio));
 		}
+	}
 
 	rc = mdss_dsi_get_bridge_chip_params(pinfo, ctrl_pdata, pdev);
 	if (rc) {
